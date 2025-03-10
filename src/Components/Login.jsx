4eb3ab2d7d25576adae3b898c82d2c4c37879e4f
ivan <oraionstarr@gmail.com>
2025-03-10 22:51:25 +0800

    login styling

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { supabase } from "../supabase";
+import "./styles.css";
+import login_img from "../assets/login_bg.jpeg";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-  const navigate = useNavigate(); // For navigating between pages
+  const navigate = useNavigate();
 
   const handleLogin = async () => {
     if (!email) {
@@ -29,29 +31,40 @@ export default function Login() {
       setError(error.message);
     } else {
       console.log(`User logged in: ${user.user_metadata.username}`);
-      navigate("/"); // Redirect to home (ParcelStatus)
+      navigate("/");
     }
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      {error && <p style={{ color: "red" }}>{error}</p>}
-      <input
-        type="email"
-        placeholder="Email"
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <br />
-      <br />
-      <input
-        type="password"
-        placeholder="Password"
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <br />
-      <br />
-      <button onClick={handleLogin}>Login</button>
+    <div className="login-container">
+      <div className="login-form-container">
+        <div className="login-form">
+          <div className="login_content">
+            <h2>Login</h2>
+            {error && <p className="login_error">{error}</p>}
+            <div className="login_input_fields">
+              <input
+                type="email"
+                placeholder="Email"
+                onChange={(e) => setEmail(e.target.value)}
+                className="login_input"
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+                className="login_input"
+              />
+            </div>
+          </div>
+          <button onClick={handleLogin} className="login-button">
+            Login
+          </button>
+        </div>
+      </div>
+      <div className="login-image-container">
+        <img className="login_img" src={login_img} alt="Login Image" />
+      </div>
     </div>
   );
 }
